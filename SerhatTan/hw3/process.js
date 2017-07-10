38f549357436918ef2e2a69d980382c60156c124
Serhat Tan <94serhattan@gmail.com>
2017-07-11 00:13:53 +0300

    [Serhat Tan] some textual improvements

--- SerhatTan/hw3/process.js
+++ SerhatTan/hw3/process.js
@@ -1,43 +1,43 @@
-//Listening the some dom elements and fetch their values
-//Calling operations function from Calculator class
-//Sending values to operations function parameters
-//Clear screen with click function event
-
-
-
-var classname = document.getElementsByClassName("btn");
+/**
+	Listening the some dom elements and fetch their values
+	Calling operations function from Calculator class
+	Sending values to operations function parameters
+	Clear screen with click function event
+*/
 var digits = [];
+var classname = document.getElementsByClassName("btn");
+var equalButton = document.getElementById("equalButton");
+var clearButton = document.getElementById("clearButton");
+
 
 digitProcess = function(e) {
-	// e.target.innerHTML is the button's number like 5, 6 
-	digits.push(e.target.innerHTML);
+	digits.push(e.target.innerHTML); // e.target.innerHTML is the button's number like 5, 6 
 	var number = digits.join("");
 	document.getElementById("screen").innerHTML = number;
 };
 
 calculate = function() {
 	var screen = document.getElementById("screen");
+	var calculator = new Calculator();
 	data = screen.innerHTML;
 
-	console.log(data.indexOf("+"));
 	// Gets the first index where a operand occours
-	if (data.indexOf("+")!=-1) {
+	if (data.indexOf("+") != -1) {
 		var index = data.indexOf("+");
 		operation = "sum";
-	} else if (data.indexOf("-")!=-1) {
-		var index = data.indexOf("-");
+	} else if (data.indexOf("-") != -1) {
+		index = data.indexOf("-");
 		operation = "subtraction";
-	} else if (data.indexOf("*")!=-1) {
-		var index = data.indexOf("*");
+	} else if (data.indexOf("*") != -1) {
+		index = data.indexOf("*");
 		operation = "multiply";
-	} else if (data.indexOf("/")!=-1) {
-		var index = data.indexOf("/");
+	} else if (data.indexOf("/") != -1) {
+		index = data.indexOf("/");
 		operation = "division";
 	}
-	var number1 = data.substr(0, index); // Gets the first part
-	var number2 = data.substr(index + 1).slice(0, -1); // Gets the second part
+	var number1 = data.substr(0, index); // Gets the first part of number
+	var number2 = data.substr(index + 1).slice(0, -1); // Gets the second part of number
 
-	var calculator = new Calculator();
 	result = calculator[operation](number1, number2);
 	screen.innerHTML = result;
 	digits = [];
@@ -54,9 +54,5 @@ for (var i = 0; i < classname.length; i++) {
 	classname[i].addEventListener('click', digitProcess, false);
 }
 
-
-var equalButton = document.getElementById("equalButton");
 equalButton.addEventListener('click', calculate, false);
-
-var clearButton = document.getElementById("clearButton");
 clearButton.addEventListener('click', clearScreen, false);
