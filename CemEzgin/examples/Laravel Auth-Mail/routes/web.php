c8c8f902c99cafca4b0eebf7c4f74b56f21874ec
cemezgn <cemezgn@gmail.com>
2017-07-28 13:41:18 +0300

    [Cem Ezgin] Ignored nodeJS modules & deleted some comment lines.

--- CemEzgin/examples/Laravel Auth-Mail/routes/web.php
+++ CemEzgin/examples/Laravel Auth-Mail/routes/web.php
@@ -15,19 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-/*Route::get('/v1/test/{name?}/{phone?}', 'HomeController@index');
-Route::get('/persons', 'HomeController@persons');
-Route::get('/person-insert', 'HomeController@insert');
-Route::get('/person-save/{id?}', 'HomeController@save');
-Route::get('/person-delete/{id}', 'HomeController@delete');
-Route::post('/personsave', 'HomeController@savePerson');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');*/
-
 Route::get('/restaurant/{name}', '\App\Modules\Restaurant\Controller@restaurantList');
